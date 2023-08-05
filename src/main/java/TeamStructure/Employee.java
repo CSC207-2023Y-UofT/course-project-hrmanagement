e64e221cd82dfb46a4135e25f973d80d8f4628cf
sarahhe25 <sarahhe125@gmail.com>
2023-08-04 23:58:11 -0400

    resolve errors

--- src/main/java/TeamStructure/Employee.java
+++ src/main/java/TeamStructure/Employee.java
@@ -1,14 +1,22 @@
 package TeamStructure;
 
-public class Employee extends User {
+/**
+ * An employee of the team. Has a wage. Can create availability or create time off requests.
+ */
+public class Employee extends User{
     private double wage;
 
-    public Employee(String username, String password, double wage) {
+    public Employee(String username, String password, double wage)
+    {
         super(username, password);
         this.wage = wage;
+    };
+
+    /**
+     * Returns the Employee's Wage rate.
+     * @return wage
+     */
+    public double getWage() {
+        return wage;
     }
-  
-  public double getWage() {
-    return wage;
-  }
 }

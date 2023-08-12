083be360110abe85ab511f18dea514b899e11360
sarahhe25 <sarahhe125@gmail.com>
2023-08-12 17:11:54 -0400

    fixed csvtimesheet, included documentation to clear up its usage

--- src/main/java/Payroll/dao/CSVStrategy.java
+++ src/main/java/Payroll/dao/CSVStrategy.java
@@ -6,8 +6,9 @@ import java.util.Map;
 
 /**
  * Implementation of DataAccessStrategy for loading data from CSV files.
- * Note that CSV Files are designed as backup data for running payroll feature, not automatically updated
- * Data persistence is implemented in MYSQL database
+ * Note that CSV Files are designed as backup data for running payroll feature's employee list if MYSQL doesn't work
+ * CSVTimesheet supports initial reading from file, but is not automatically updated
+ * Data persistence is implemented through MYSQL database
  */
 public class CSVStrategy implements DataAccessStrategy {
 

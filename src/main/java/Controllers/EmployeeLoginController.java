76fd743478a8c6a621c162b7d3ba5d4ed3dcd5f5
samreengill <samreengill.kaur@mailutoronto.ca>
2023-08-13 01:53:07 -0400

    edited controller

---
+++ src/main/java/Controllers/EmployeeLoginController.java
@@ -0,0 +1,18 @@
+package Controllers;
+
+import UseCase.*;
+
+public class EmployeeLoginController {
+
+    final EmployeeLoginInputBoundary empInput;
+
+    public EmployeeLoginController(EmployeeLoginInputBoundary accGateway){
+        this.empInput = accGateway;
+    }
+
+    public EmployeeLoginResponseModel checkLogin(int employeeID, String password){
+        EmployeeLoginRequestModel requestModel = new EmployeeLoginRequestModel(employeeID, password);
+
+        return empInput.checkLogin(requestModel);
+    }
+}

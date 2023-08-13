e24cf1126d8c494e561e54b8b1a9a0a0d9617cf4
Ali <ali.patel@utoronto.ca>
2023-08-13 17:04:01 -0400

    Timeoff Refactoring

--- src/main/java/UI/ManagerPortalBox.java
+++ src/main/java/UI/ManagerPortalBox.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.ParseException;
 import Schedule.*;
-import TimeOff.ManagerTimeOff;
 import TimeOff.TimeOffGUI;
 
 /**

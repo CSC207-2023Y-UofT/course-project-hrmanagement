64879f67b620520cf6d8d8b8189f0d49fdaec8b7
samreengill <samreengill.kaur@mailutoronto.ca>
2023-08-13 02:22:50 -0400

    edited main

--- src/main/java/HRManagement.java
+++ src/main/java/HRManagement.java
@@ -1,13 +1,19 @@
+import Entities.CommonEmployeeFactory;
+import Entities.EmployeeFactory;
 import UI.PositionBox;
 
+/**
+ * The main method of the HRManagement application.
+ */
 public class HRManagement {
-
-    static void viewPositionBox(){
-        PositionBox positionBox = new PositionBox();
-        positionBox.createChoosePositionBox();
-    }
-
     public static void main(String[] args) {
-        viewPositionBox();
+        final String URL = "jdbc:mysql://localhost:3306/employees";
+        final String USERNAME = "root";
+        final String PASSWORD = "password";
+        EmployeeFactory employeeFactory = new CommonEmployeeFactory();
+
+        /*Choose Position Portal*/
+        PositionBox positionBox = new PositionBox();
+        positionBox.createChoosePositionBox(URL, USERNAME, PASSWORD, employeeFactory);
     }
 }

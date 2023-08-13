61ea8c06ddfde3bf26491d3b2c5a749785d6921f
samreengill <samreengill.kaur@mailutoronto.ca>
2023-08-13 01:56:34 -0400

    changed employee

--- src/main/java/UI/EmployeeBox.java
+++ src/main/java/UI/EmployeeBox.java
@@ -1,70 +1,77 @@
 package UI;
 
+import Controllers.EmployeeLoginController;
+import Controllers.EmployeeLoginResponseFormatter;
+import Controllers.EmployeeRegisterController;
+import Controllers.EmployeeRegisterResponseFormatter;
+import Database.EmployeeLoginDBGateway;
+import Database.EmployeeRegisterDBGateway;
+import Database.FileEmployee;
+import Database.FileLoginEmployee;
+import Entities.EmployeeFactory;
+import Database.EmployeeLoginDBGateway;
+import UseCase.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-/**
- * The EmployeeBox class allows a user to choose to 'signUp' or 'login'.
- * It creates a JFrame displaying two buttons - for 'signUp' and 'login'.
- * The user can click on the signUp button to open Employee signUp form or
- * the login button to open login frame.
- */
+import java.sql.SQLException;
 
 public class EmployeeBox {
     Button signUp;
     Button login;
     MyJFrame frame;
     Panel panel;
-    Button back;
 
     /**
      * This method creates all UI components of EmployeeBox
      */
-
-    public void createEmployeeBox(){
-
-        // creates JFrame
+    public void createEmployeeBox(String url, String userName, String password, EmployeeFactory employeeFactory){
         frame = new MyJFrame();
         frame.createFrame("Employees", 300, 300);
 
-        // create a panel
         panel = new Panel();
         panel.createPanel(20, 30, 20, 30, "GridLayout", 0, 1, 10, 10, frame.getJFrame());
 
-        // create button for signup
         signUp = new Button();
         signUp.createButtonWithIcon(frame.getJFrame(), "SignUp", 50, 50, 200, 50);
 
-        // create button for login
         login = new Button();
         login.createButtonWithIcon(frame.getJFrame(), "Login", 50, 120, 200, 50);
-
-        // Back button to return
-        back = new Button();
-        back.createButtonWithIcon(frame.getJFrame(), "< Back to Selection", 1, 200, 200, 20);
 
         // action when signUp button is clicked
         signUp.getButton().addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
-//                new EmployeeForm().setVisible(true);
+            public void actionPerformed(ActionEvent evt) {
+                EmployeeRegisterDBGateway employeeSignUp;
+                try{
+                    employeeSignUp = new FileEmployee(url, userName, password);
+                }catch(SQLException e) {
+                    e.printStackTrace();
+                    employeeSignUp = null;
+                }
+
+                EmployeeRegisterPresenter presenter = new EmployeeRegisterResponseFormatter();
+                EmployeeRegisterInputBoundary interactor = new EmployeeRegisterInteractor(employeeSignUp, presenter, employeeFactory);
+                EmployeeRegisterController employeeRegisterController = new EmployeeRegisterController(interactor);
+                EmployeeSignUpForm employeeSignUpForm = new EmployeeSignUpForm(employeeRegisterController);
+
             }
         });
 
         // action when login button is clicked
         login.getButton().addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                new EmployeeSignInBox().createEmployeeBox();
-                frame.getJFrame().dispose();
-            }
-        });
-
-        back.getButton().addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new PositionBox().createChoosePositionBox();
-                frame.getJFrame().dispose();
+            public void actionPerformed(ActionEvent evt) {
+                EmployeeLoginDBGateway employeeLoginDBGateway;
+                try {
+                    employeeLoginDBGateway = new FileLoginEmployee(url, userName, password);
+                }catch (SQLException e){
+                    e.printStackTrace();
+                    employeeLoginDBGateway = null;
+                }
+                EmployeeLoginPresenter employeeLoginPresenter = new EmployeeLoginResponseFormatter();
+                EmployeeLoginInputBoundary interactorInput = new EmployeeLoginInteractor(employeeLoginDBGateway, employeeLoginPresenter, employeeFactory);
+                EmployeeLoginController employeeLoginController = new EmployeeLoginController(interactorInput);
+                EmployeeSignInBox employeeSignInBox = new EmployeeSignInBox(employeeLoginController);
             }
         });
     }
